de2380d4b292f26ee525c775a2cae026231ff8e4
frankg <frankg@sliq.com>
2019-05-31 15:41:16 -0400

    s2

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -1,2 +1,3 @@
 export './loading_indicator.dart';
 export './food_item.dart';
+export './placeholder.dart';
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:parl_cuision/authentication/authentication.dart';
-import 'package:parl_cuision/common/food_item.dart';
+import 'package:parl_cuision/common/common.dart';
+import 'package:parl_cuision/menu/menu.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -15,13 +16,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  bool _filterItem1 = true;
-  bool _filterItem2 = true;
-  bool _filterItem3 = true;
-
-  void _filterItem1Changed(bool v) => setState(() => _filterItem1 = v);
-  void _filterItem2Changed(bool v) => setState(() => _filterItem2 = v);
-  void _filterItem3Changed(bool v) => setState(() => _filterItem3 = v);
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    MenuPage(),
+    PlaceholderWidget(Colors.white),
+    PlaceholderWidget(Colors.deepOrange),
+    PlaceholderWidget(Colors.green)
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,169 +30,34 @@ class _HomePageState extends State<HomePage> {
         BlocProvider.of<AuthenticationBloc>(context);
 
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Column(
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 20.0, top: 40.0,),
-                alignment: Alignment.topLeft,
-                color: Colors.white,
-                child: Text("Menu",style: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),),
-              ),
-
-              Container(
-                padding: EdgeInsets.only(left: 20.0, top: 40.0, bottom: 15.0,),
-                alignment: Alignment.topLeft,
-                color: Colors.white,
-                child: Text("Filters",style: TextStyle( color: Colors.grey, fontSize: 14.0, fontWeight: FontWeight.w600,),),
-              ),
-
-              Container(
-                padding: EdgeInsets.only(left: 12.0, top: 0.0,),
-                margin: EdgeInsets.all(0.0),
-                height: 80,
-                alignment: Alignment.centerLeft,
-                decoration: new BoxDecoration(
-                  color: Colors.white,
-                  border: new Border(top: BorderSide(color: Colors.grey[200],width: 0.8,))
-                ),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    GestureDetector(
-                      onTap: () => _filterItem1Changed(!_filterItem1),
-                      child: Container(
-                        padding: EdgeInsets.only(right: 8.0, left: 8.0,),
-                        decoration: new BoxDecoration(border: Border(right:BorderSide(color: Colors.grey[200], width:0.8,),),),
-                        child: Stack(
-                          children: <Widget>[
-                            Container(height: 80, width: 60,),
-                            Positioned(
-                              top: 4.0,
-                              right: 0,
-                              child: Image.asset(_filterItem1?"assets/images/checked.png":"assets/images/unchecked.png",height: 13,width: 13,),
-                            ),
-                            Align(
-                              alignment: Alignment.center,
-                              child: Container(
-                                child: Image.asset("assets/images/a.png",height: 55.0, width: 50.0,),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () => _filterItem2Changed(!_filterItem2),
-                      child: Container(
-                        padding: EdgeInsets.only(right: 8.0, left: 8.0,),
-                        decoration: new BoxDecoration(border: Border(right:BorderSide(color: Colors.grey[200], width:0.8,),),),
-                        child: Stack(
-                          children: <Widget>[
-                            Container(height: 80, width: 60,),
-                            Positioned(
-                              top: 4.0,
-                              right: 0,
-                              child: Image.asset(_filterItem2?"assets/images/checked.png":"assets/images/unchecked.png",height: 13,width: 13,),
-                            ),
-                            Align(
-                              alignment: Alignment.center,
-                              child: Container(
-                                child: Image.asset("assets/images/b.png",height: 55.0, width: 75.0,),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () => _filterItem3Changed(!_filterItem3),
-                      child: Container(
-                        padding: EdgeInsets.only(right: 8.0, left: 8.0,),
-                        // decoration: new BoxDecoration(border: Border(right:BorderSide(color: Colors.grey[200], width:0.8,),),),
-                        child: Stack(
-                          children: <Widget>[
-                            Container(height: 80, width: 60,),
-                            Positioned(
-                              top: 4.0,
-                              right: 0,
-                              child: Image.asset(_filterItem3?"assets/images/checked.png":"assets/images/unchecked.png",height: 13,width: 13,),
-                            ),
-                            Align(
-                              alignment: Alignment.center,
-                              child: Container(
-                                child: Image.asset("assets/images/c.png",height: 45.0, width: 55.0,),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              
-              Container(
-                margin: EdgeInsets.only(top: 20.0, left: 20.0,),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.expand_more),
-                    Text("Appetizers",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,),),
-                  ],
-                ),
-              ),
-              
-              Expanded(
-                child: Container(child: FoodCardList(),),
-              ),
-              // Expanded(child: Container(color: Colors.cyan,),),
-            ],
-          ),
-          Positioned(
-            top: 50,
-            right: 0.0,
-            child: Container(
-              child: Image.asset("assets/images/menu_plate.png", ),
-            ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-          child: Container(
-        height: 80,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            IconButton(
-              icon: new Image.asset("assets/images/icon_menu.png"),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: new Image.asset("assets/images/icon_reservation.png"),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: new Image.asset("assets/images/icon_info.png"),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: new Image.asset("assets/images/icon_cart.png"),
-              onPressed: () {},
-            ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: onTabTapped,
+          currentIndex: _currentIndex,
+          // iconSize: 18.0,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            new BottomNavigationBarItem(icon: getImageIconBox('assets/images/icon_menu.png'), title: Text("Menu")),
+            new BottomNavigationBarItem(icon: getImageIconBox("assets/images/icon_reservation.png"),title: Text("Resv")),
+            new BottomNavigationBarItem(icon: getImageIconBox("assets/images/icon_info.png"), title: Text("Info")),
+            new BottomNavigationBarItem(icon: getImageIconBox("assets/images/icon_cart.png"), title: Text("Cart")),
           ],
-        ),
-      )),
-      // floatingActionButton: Container(
-      //   padding: EdgeInsets.only(top: 180.0, right: 0.0),
-      //   child: Align(
-      //     alignment: Alignment.topRight,
-      //     child: Image.asset("assets/images/menu_plate.png", ),
-      //   ),
-      // ),
-      
+      ),
     );
   }
 
+  void onTabTapped(int index) {
+    setState(() {
+     _currentIndex = index;
+    });
+  }
+
+  Widget getImageIconBox(String path){
+    return SizedBox(
+      child: Image.asset(path),
+      width: 24,
+      height: 24,
+    );
+  }
 }
